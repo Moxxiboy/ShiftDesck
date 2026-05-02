c22960bdf18ba06ca5077e20eee51fc27d6dc612
Moxxiboy <gsc5gd7n59@privaterelay.appleid.com>
2026-05-02 08:38:35 +0300

    Delete shiftdesk_update directory

--- shiftdesk_update/scripts/generate_vapid_keys.py
+++
@@ -1,9 +0,0 @@
-from py_vapid import Vapid
-
-vapid = Vapid()
-vapid.generate_keys()
-
-print("Add these Environment Variables in Render:")
-print("VAPID_PUBLIC_KEY=" + vapid.public_key)
-print("VAPID_PRIVATE_KEY=" + vapid.private_key)
-print("VAPID_SUB=mailto:your-email@example.com")
